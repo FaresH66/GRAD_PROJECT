84b137e6f0859b810b4a15dbf92b694b1d9ee2a3
FaresH66 <154681478+FaresH66@users.noreply.github.com>
2025-03-25 23:10:54 +0000

    added database previleage config in mysql-cli

--- db/install_db.sh
+++ db/install_db.sh
@@ -5,3 +5,8 @@ sudo apt install mysql-server
 sudo service mysql status
 sudo service mysql start
 sudo service mysql status
+
+
+
+# ALTER USER 'root'@'localhost' IDENTIFIED WITH 'mysql_native_password' BY 'myrootpass123';
+# FLUSH PRIVILEGES;
